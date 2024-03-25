cc1b81a51abba3387c54d854865b971eaa69baea
Katitamorais <163673653+Katitamorais@users.noreply.github.com>
2024-03-25 17:38:32 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -17,3 +17,8 @@ Digite um número:
 
 Dica: Lembre do comando de resto da divisão inteira!
 """
+usernumber=int(input('Digite um número inteiro:'))
+if (usernumber%2) == 0:
+    print('Par')
+else:
+    print('Ímpar')
