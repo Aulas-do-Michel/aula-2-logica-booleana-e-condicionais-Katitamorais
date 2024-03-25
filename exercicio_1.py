5c2ad5c8ab8cc366e1eadd44cbaf608ea7148795
Katitamorais <163673653+Katitamorais@users.noreply.github.com>
2024-03-25 17:43:33 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -1,22 +1,3 @@
-"""
-#### Exercício 1
-
-Receba um número inteiro de um usuário. Se ele for par, imprima "Par". Se não, imprima "Ímpar".
-
-Exemplo:
-
-Digite um número:
-10
-
-Par
---------
-Digite um número:
-1
-
-Ímpar
-
-Dica: Lembre do comando de resto da divisão inteira!
-"""
 usernumber=int(input('Digite um número inteiro:'))
 if (usernumber%2) == 0:
     print('Par')
